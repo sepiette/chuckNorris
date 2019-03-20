f1abe7d70f2bd8a5d758f06f5bdb0966d10ff2e8
THW8526 <sara.piette@kroger.com>
2019-03-19 23:15:24 -0400

    add authentication to dashboard route

--- src/main/webapp/app/dashboard/dashboard.route.ts
+++ src/main/webapp/app/dashboard/dashboard.route.ts
@@ -1,12 +1,14 @@
 import { Route } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { UserRouteAccessService } from 'app/core';
 
 export const DASHBOARD_ROUTE: Route = {
     path: 'dashboard',
     component: DashboardComponent,
     data: {
-        authorities: [],
+        authorities: ['ROLE_USER'],
         pageTitle: 'Chuck Norris Dashboard'
-    }
+    },
+    canActivate: [UserRouteAccessService]
 };
